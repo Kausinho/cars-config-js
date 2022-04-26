be9aabf825d1d171557311dde3a0429671ac49e7
Kausinho <kausinhobrunow@gmail.com>
2022-04-26 16:28:32 -0300

    Create HeaderCarros.vue

---
+++ src/components/HeaderCarros.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <form action="">
+            <h2>LOGOMARCA</h2>
+             <ul>
+                 <li><a href="">Home</a></li>
+                 <li><a href="">Carros</a></li>
+                 <li><a href="">Contato</a></li>
+             </ul>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderCarros"
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
