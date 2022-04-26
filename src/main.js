4d1a08039939f14df718906fc5c0ea87138f9dc5
Kausinho <kausinhobrunow@gmail.com>
2022-04-26 16:28:53 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import PrimeIcons from 'primeicons/primeicons.css';
 
 createApp(App).mount('#app')
+
+App.use(PrimeVue);
+App.use(PrimeIcons);
